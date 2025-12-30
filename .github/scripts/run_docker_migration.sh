d33c4c90ee8ac51f296cffe2f3243e383728d498
varunjain-byte <varruunjain@gmail.com>
2025-12-30 11:37:57 +0530

    Delete .github directory

--- .github/scripts/run_docker_migration.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd $SSH_PROJECT_DIR
-git pull
-make migrate
-echo "Migration complete"
--- .github/scripts/run_src_migration.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd $SSH_PROJECT_DIR
-git pull
-source .venv/bin/activate
-alembic -c src/backend/alembic.ini upgrade head
-echo "Migration complete"
